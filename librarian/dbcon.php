1dcef695f86a0274225c582436ea298c2454c4cd
mwangi <chiebyrah@gmail.com>
2024-07-04 18:51:30 +0300

    updated everywhere there was an old db

--- librarian/dbcon.php
+++ librarian/dbcon.php
@@ -1,3 +1,17 @@
 <?php
-mysql_select_db('eb_lms',mysql_connect('localhost','root',''))or die(mysql_error());
+// Create connection
+$connection = mysqli_connect('localhost', 'root', '49324932', 'myo_lms_db');
+
+// Check connection
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Select database (optional, since it's already selected in the connection)
+mysqli_select_db($connection, 'myo_lms_db') or die(mysqli_error($connection));
+
+// Your code here...
+
+// Close the connection when done
+mysqli_close($connection);
 ?>
--- library/db/eb_lms.sql
+++ library/db/eb_lms.sql
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `eb_lms`
+-- Database: `myo_lms_db`
 --
 
 -- --------------------------------------------------------
