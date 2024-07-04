e0f40f1b0fa4edecab98032799a854e15c9d0086
mwangi <chiebyrah@gmail.com>
2024-07-04 18:03:21 +0300

    upgraded my mysql to msqli

--- dbcon.php
+++ dbcon.php
@@ -1,3 +1,14 @@
 <?php
-mysql_select_db('jnv',mysql_connect('localhost','root',''))or die(mysql_error());
+$host = 'localhost';
+$username = 'root';
+$password = '49324932';
+$database = 'myo lms db';
+
+// Create connection
+$conn = mysqli_connect($host, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
